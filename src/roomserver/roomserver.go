bc1b5e470a1f1083f335b2651e88fc4e30b0f654
CapsLiang <liangshuangplus@outlook.com>
2021-04-29 20:05:27 +0800

    [roomserver] 通过gonet提供的接口来运行roomserver

--- src/roomserver/roomserver.go
+++ src/roomserver/roomserver.go
@@ -1,9 +1,12 @@
 package roomserver
 
 import (
+	"base/env"
 	"base/gonet"
+	"flag"
 	"github.com/golang/glog"
 	"github.com/gorilla/websocket"
+	"time"
 )
 
 type RoomServer struct {
@@ -33,4 +36,52 @@ func RoomServer_GetMe() *RoomServer {
 func (r RoomServer) OnWSAccept(conn *websocket.Conn) {
 	//Todo: NewPlayerTask(conn).Start()
 	glog.Info("[WS] Connected websocket已连接")
+}
+
+func (r RoomServer) Init() bool {
+	//todo: RoomGrpcClient_GetMe().Init() 通过grpc获取此房间的ip port
+
+	go func() {
+		//开一条goroutine 通过gonet提供的接口读取配置并监听
+		err := r.roomser.WSBind(env.Get("room", "listen"))
+		if nil != err {
+			glog.Error("[Start] Bind Port Fail")
+		}
+	}()
+
+	return true
+}
+
+func (r RoomServer) Reload() {
+}
+
+func (r RoomServer) MainLoop() {
+	//可以不用
+	time.Sleep(time.Second)
+}
+
+func (r RoomServer) Final() bool {
+	//todo: RoomGrpcClient_GetMe().Close()
+	return true
+}
+
+//执行时通过命令行初始化配置环境
+var (
+	config  = flag.String("config", "", "config file")
+	logfile = flag.String("logfile", "", "log file")
+)
+
+func main() {
+	flag.Parse()
+
+	env.Load(*config)
+
+	if "" != *logfile {
+		glog.SetLogFile(*logfile)
+	} else {
+		glog.SetLogFile(env.Get("room", "log"))
+	}
+	defer glog.Flush()
+
+	RoomServer_GetMe().Main()
 }
