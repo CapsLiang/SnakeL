28bcfecc66b4b55220a1af8062138f2e00ff1470
CapsLiang <liangshuangplus@outlook.com>
2021-04-29 19:56:32 +0800

    [roomserver] 通过gonet提供的接口来接收websocket

--- src/roomserver/roomserver.go
+++ src/roomserver/roomserver.go
@@ -1,1 +1,36 @@
 package roomserver
+
+import (
+	"base/gonet"
+	"github.com/golang/glog"
+	"github.com/gorilla/websocket"
+)
+
+type RoomServer struct {
+	gonet.Service
+	roomser *gonet.WebSocketServer
+}
+
+var mServer *RoomServer
+
+func RoomServer_GetMe() *RoomServer {
+	if nil == mServer {
+		mServer = &RoomServer{
+			Service: gonet.Service{},
+			roomser: &gonet.WebSocketServer{},
+		}
+	}
+
+	// 需要实现  Init() Reload() MainLoop() Final() 接口
+	mServer.Derived = mServer
+
+	//需要实现OnWSAccept方法
+	mServer.roomser.Derived = mServer
+
+	return mServer
+}
+
+func (r RoomServer) OnWSAccept(conn *websocket.Conn) {
+	//Todo: NewPlayerTask(conn).Start()
+	glog.Info("[WS] Connected websocket已连接")
+}
