396631fe5ca6ed5839d0ddf8659b6270c47e39fb
CapsLiang <liangshuangplus@outlook.com>
2021-05-06 17:08:28 +0800

    [scene] 更改了蛇身结构食物结构 更新其他玩家信息 序列化场景信息

--- src/roomserver/scene.go
+++ src/roomserver/scene.go
@@ -8,94 +8,132 @@ import (
 )
 
 type SnakeBody struct {
-	Id   uint32
-	Name string
-	Head common.POINT
-	Body []common.POINT
+	id         uint32
+	name       string
+	thisplayer *PlayerTask
+	direct     uint32
+	head       common.POINT
+	body       []common.POINT
+	//invincible bool todo 无敌
 }
 
+type FoodList struct {
+	//foodMutex sync.Mutex
+	foodlist []common.Food //食物列表
+	eatfood  map[int]bool  //判断是否被吃 每次更新
+}
+
+//公用食物
+var mFoods *FoodList
+
 type Scene struct {
-	room *Room
+	room *Room //所属房间
 
-	head common.POINT   //蛇头的位置
-	body []common.POINT //蛇身
-	//headnext   common.POINT   //蛇头下次移动的位置
-	invincible bool //无敌
-	//pool *BallPool //后续做不同类型的食物
-
-	foodlist []common.POINT //食物列表
-	eatfood  map[int]bool   //判断是否被吃 每次更新
+	snake SnakeBody //本条蛇
 
 	others []SnakeBody //其他人的信息
 
 	preTime int64 //上一帧时间
 }
 
-func (this *Scene) FoodList_GetMe() []common.POINT {
-	if nil == this.foodlist {
-		this.foodlist = make([]common.POINT, 0, common.FoodNum)
+func (this *Scene) AddFoods() {
+	//不存在时 创建食物数组
+	if nil == mFoods {
+		mFoods = &FoodList{
+			//foodMutex: sync.Mutex{},
+			foodlist: make([]common.Food, 0, common.FoodNum),
+			eatfood:  make(map[int]bool),
+		}
 		//初始化食物数组
-		for i := 0; i <= int(common.FoodNum); i++ {
+		for i := 0; i < int(common.FoodNum); i++ {
 			//并未被吃
-			this.eatfood[i] = false
+			mFoods.eatfood[i] = false
 			x, y := common.RandPOINTFloat64()
-			this.foodlist = append(this.foodlist, common.POINT{
-				X: x - 10,
-				Y: y - 10,
+			mFoods.foodlist = append(mFoods.foodlist, common.Food{
+				Energy: common.FoodEnergy,
+				Stat:   common.POINT{X: x - 10, Y: y - 10},
 			})
 		}
-
 	}
-	return this.foodlist
-}
-
-//添加食物
-func (this *Scene) AddFoods() {
-	if len(this.foodlist) < cap(this.foodlist) {
+	//食物未生成够 添加食物
+	if len(mFoods.foodlist) < cap(mFoods.foodlist) {
 		//当容量小于 最大容量
-		for i := 0; i <= int(common.FoodNum); i++ {
+		for i := 0; i < int(common.FoodNum); i++ {
 			//食物已经被吃了 生成新food
-			if !this.eatfood[i] {
-				this.eatfood[i] = true
+			if mFoods.eatfood[i] {
 				x, y := common.RandPOINTFloat64()
-				this.foodlist[i] = common.POINT{
-					X: x - 10,
-					Y: y - 10,
+				mFoods.foodlist[i] = common.Food{
+					Energy: common.FoodEnergy,
+					Stat:   common.POINT{X: x - 10, Y: y - 10},
 				}
+				mFoods.eatfood[i] = false
 			}
 		}
 	}
 }
 
-func (this *Scene) EatFood() {
+func (this *Scene) GetFoodList() *FoodList {
+	return mFoods
+}
 
+func (this *Scene) EatFood() {
+	//todo this.snake.Head
 }
 
 func (this *Scene) InitSnake() {
 	headx, heady := common.RandPOINTFloat64()
 	temhead := common.POINT{
-		X: headx - 20,
-		Y: heady - 20,
+		X: headx - 50,
+		Y: heady - 50,
 	}
 
-	this.head = temhead
+	this.snake.id = this.snake.thisplayer.id
+	this.snake.name = this.snake.thisplayer.name
+	this.snake.direct = this.snake.thisplayer.angle
 
+	this.snake.head = temhead //初始化新生成的头
+	//身体 默认向右移动
 	for i := 1; i <= 3; i++ {
-		this.body = append(this.body, common.POINT{
+		this.snake.body = append(this.snake.body, common.POINT{
 			X: temhead.X - 2*float64(i)*common.SnakeRadius,
-			Y: temhead.X - 2*float64(i)*common.SnakeRadius,
+			Y: temhead.Y,
 		})
 	}
+	//this.snake.invincible = true //无敌
 
 }
 
 func (this *Scene) UpdateSnakePOINT(angle uint32) {
-
-	//space := common.SceneSpeed * float64(time.Now() )
-	space := common.SceneSpeed * float64(((time.Now().UnixNano()/1e6)-(this.preTime))/1000) //相差毫秒 / 1000 每秒
+	space := common.SceneSpeed * float64(((time.Now().UnixNano()/1e6)-(this.preTime))/1000) //相差(毫秒 / 1000) 即每秒
 	this.preTime = (time.Now().UnixNano() / 1e6)                                            //毫秒
 
 	this.SnakeMove(angle, space)
+}
+
+//todo 传的是引用吗?
+func (this *Scene) UpdateOthersSnake() {
+
+	for _, p := range this.room.players {
+		if p.scene.others == nil {
+			p.scene.others = []SnakeBody{}
+		}
+
+		for _, other := range this.room.players {
+			//如果不是本身的那一条连接
+			if p.id != other.id {
+				p.scene.others = append(p.scene.others, SnakeBody{
+					id:         other.scene.snake.id,
+					name:       other.scene.snake.name,
+					thisplayer: other.scene.snake.thisplayer,
+					direct:     0,
+					head:       other.scene.snake.head,
+					body:       other.scene.snake.body,
+				})
+
+			}
+		}
+	}
+
 }
 
 func (this *Scene) SnakeMove(angle uint32, space float64) {
@@ -113,9 +151,40 @@ func (this *Scene) SnakeBodyMove(newhead common.POINT) {
 }
 
 func (this *Scene) SceneMsg() []byte {
+
 	//todo 序列化场景信息
 	var retsceneMsg common.RetSceneMsg
-	retsceneMsg = common.RetSceneMsg{}
+	//先分配空间
+	retsceneMsg.PlayerSnake = []common.RetSnakeBody{}
+	retsceneMsg.OthersSnake = []common.RetSnakeBody{}
+	retsceneMsg.FoodList = []common.Food{}
+
+	//本条蛇
+	retsceneMsg.PlayerSnake = append(retsceneMsg.PlayerSnake, common.RetSnakeBody{
+		Id:   this.snake.id,
+		Name: this.snake.name,
+		Head: this.snake.head,
+		Body: this.snake.body,
+	})
+	//其他蛇
+	for _, other := range this.others {
+		retsceneMsg.OthersSnake = append(retsceneMsg.OthersSnake, common.RetSnakeBody{
+			Id:   other.id,
+			Name: other.name,
+			Head: other.head,
+			Body: other.body,
+		})
+	}
+	//食物
+	for i := 0; i <= int(common.FoodNum); i++ {
+		//发送所有没被吃的食物
+		if !mFoods.eatfood[i] {
+			retsceneMsg.FoodList = append(retsceneMsg.FoodList, common.Food{
+				Energy: mFoods.foodlist[i].Energy,
+				Stat:   mFoods.foodlist[i].Stat,
+			})
+		}
+	}
 
 	bytes, err := json.Marshal(retsceneMsg)
 	if nil != err {
